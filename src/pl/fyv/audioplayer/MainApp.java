a8dc7b7fa3fa997e252acc33e8e94d0aa082956e
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-02-17 16:12:11 +0100

    Initial commit

---
+++ src/pl/fyv/audioplayer/MainApp.java
@@ -0,0 +1,16 @@
+package pl.fyv.audioplayer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
